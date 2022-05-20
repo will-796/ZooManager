47f1c7c5b887054f94ff38c213e65da45e9120a7
willian portela <willianportela1@gmail.com>
2022-05-20 18:11:08 -0300

    requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
+const mondayClose = () => ({
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!' });
+
+const animalArrExibition = (day) => data.species
+  .filter(({ availability }) => availability.includes(day))
+  .map(({ name }) => name);
+
+const createValues = (day) => {
+  if (day === 'Monday') return mondayClose();
+
+  const { open, close } = data.hours[day];
+  return {
+    officeHour: `Open from ${open}am until ${close}pm`,
+    exhibition: animalArrExibition(day),
+  };
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animalArr = data.species.map(({ name }) => name);
+  const daysArr = Object.keys(data.hours);
+
+  if (scheduleTarget === 'Monday') {
+    return { Monday: mondayClose() };
+  }
+  if (daysArr.includes(scheduleTarget)) {
+    return { [scheduleTarget]: createValues(scheduleTarget) };
+  }
+  if (animalArr.includes(scheduleTarget)) {
+    return data.species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  return daysArr.reduce((obj, day) => ({ ...obj, [day]: createValues(day) }), {});
 }
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
