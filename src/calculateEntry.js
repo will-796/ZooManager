2419b047e99a463159dc4b0b15fa2612586717d3
willian portela <willianportela1@gmail.com>
2022-05-20 16:41:04 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const { adult: adultPrice, senior: seniorPrice, child: childPrice } = data.prices;
+  const { adult, senior, child } = countEntrants(entrants);
+  return adult * adultPrice + senior * seniorPrice + child * childPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
