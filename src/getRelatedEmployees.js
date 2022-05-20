74b63141c893aa97fc34697d3139d3aabdcfbf60
willian portela <willianportela1@gmail.com>
2022-05-20 15:22:01 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  try {
+    if (!isManager(managerId)) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+    return data.employees
+      .filter(({ managers }) => managers.includes(managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
