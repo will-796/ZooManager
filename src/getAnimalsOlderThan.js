5927dcdfae8d6cc7ebc89029b30ec6eae38469d0
willian portela <willianportela1@gmail.com>
2022-05-20 15:04:26 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find(({ name }) => animal === name).residents
+    .every(({ age: ageAnimal }) => age < ageAnimal);
 }
 
 module.exports = getAnimalsOlderThan;
