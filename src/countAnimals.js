fa199c6162f8e499049b8cc59893c9c209f13e84
willian portela <willianportela1@gmail.com>
2022-05-20 15:46:57 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const countSpecie = (nameAnimal) => data.species
+  .find(({ name }) => name === nameAnimal).residents.length;
+
+const countSpecieAndSex = (specie, sexAnimal) => data.species
+  .find(({ name }) => name === specie).residents
+  .filter(({ sex }) => sex === sexAnimal).length;
+
+function countAnimals(animal = {}) {
+  const { specie, sex } = animal;
+  if (sex) return countSpecieAndSex(specie, sex);
+  if (specie) return countSpecie(specie);
+
+  return data.species
+    .reduce((obj, { name }) => ({ ...obj, [name]: countSpecie(name) }), {});
 }
 
 module.exports = countAnimals;
