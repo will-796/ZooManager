828b5257333d13439917dc89490c16b49c643967
willian portela <willianportela1@gmail.com>
2022-05-23 14:33:45 -0300

    refatorando getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,57 +1,34 @@
 const data = require('../data/zoo_data');
 
-// caro leitor desse código não tive imaginação e nem capacidade para melhorar ele, sei que está horrível, e pode ser melhorado de infinitas maneiras, mas ao menos está funcionando, #feitoMelhorQuePerfeito
+// Depois de fazer code review e ter visto modos mais otimizados de fazer o codigo, refatorei e me sinto melhor, pois agora ta mais bonito e dinamico. :)
 
-const createArrBykey = (key) => data.species
-  .filter(({ location }) => location === key)
-  .map(({ name }) => name);
-
-const objNameSpecie = (nameKey) => ({ [nameKey]: data.species
-  .find(({ name }) => name === nameKey).residents
-  .map(({ name }) => name) });
-
-const objNameAndSort = (nameKey) => ({ [nameKey]: data.species
-  .find(({ name }) => name === nameKey).residents
-  .map(({ name }) => name).sort() });
-
-const objNameSex = (nameKey, sex) => ({ [nameKey]: data.species
-  .find(({ name }) => name === nameKey).residents
-  .filter(({ sex: sexfilter }) => sexfilter === sex)
-  .map(({ name }) => name) });
-
-const objNameSortAndSex = (nameKey, sex) => ({ [nameKey]: data.species
-  .find(({ name }) => name === nameKey).residents
-  .filter(({ sex: sexfilter }) => sexfilter === sex)
-  .map(({ name }) => name).sort() });
-
-const crateLocationArr = (key, sex, callback) => data.species
-  .filter(({ location }) => location === key)
-  .map(({ name }) => name)
-  .reduce((arr, nameKey) => ([...arr, callback(nameKey, sex)]), []);
-
-const objIncludeName = (callback, sex) => {
-  const arrLocations = ['NE', 'NW', 'SE', 'SW'];
-  return arrLocations
-    .reduce((obj, key) => ({ ...obj, [key]: crateLocationArr(key, sex, callback) }), {});
+const arrayNames = (sex, sorted, residents) => {
+  if (sorted && sex) {
+    return residents.filter(({ sex: filter }) => filter === sex).map(({ name }) => name).sort();
+  }
+  if (sorted) {
+    return residents.map(({ name }) => name).sort();
+  }
+  if (sex) {
+    return residents.filter(({ sex: filter }) => filter === sex).map(({ name }) => name);
+  }
+  return residents.map(({ name }) => name);
 };
 
-const createObjDefalult = () => {
-  const arrLocations = ['NE', 'NW', 'SE', 'SW'];
-  return arrLocations
-    .reduce((obj, key) => ({ ...obj, [key]: createArrBykey(key) }), {});
-};
+const createObjectNames = (sex, sorted) => data.species
+  .reduce((obj, { name, location, residents }) =>
+    ({ ...obj,
+      [location]: [...(obj[location] || []),
+        { [name]: arrayNames(sex, sorted, residents) }] }), {});
 
-const verifyConditions = (sorted, sex) => {
-  if (sorted && sex) return objIncludeName(objNameSortAndSex, sex);
-  if (sex) return objIncludeName(objNameSex, sex);
-  if (sorted) return objIncludeName(objNameAndSort);
-  return objIncludeName(objNameSpecie);
-};
+const createObjDefalult = () => data.species
+  .reduce((obj, { name, location }) =>
+    ({ ...obj, [location]: [...(obj[location] || []), name] }), {});
 
 function getAnimalMap(options = {}) {
   const { includeNames, sorted, sex } = options;
   if (!includeNames) return createObjDefalult();
-  return verifyConditions(sorted, sex);
+  if (includeNames) return createObjectNames(sex, sorted);
 }
 
 module.exports = getAnimalMap;
